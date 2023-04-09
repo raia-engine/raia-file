d69ff8c7ed537fa0e5e23b6062fc2e20b81812a2
dolphilia <42200736+dolphilia@users.noreply.github.com>
2023-04-09 18:03:29 +0900

    update

--- src/raia_file.h
+++ src/raia_file.h
@@ -1,6 +1,12 @@
 #ifndef RAIA_FILE_RAIA_FILE_H
 #define RAIA_FILE_RAIA_FILE_H
 
+#ifdef __WINDOWS__
+#define RAIA_EXPORT __declspec(dllexport) 
+#else
+#define RAIA_EXPORT
+#endif
+
 void hello(void);
 
 #endif //RAIA_FILE_RAIA_FILE_H
